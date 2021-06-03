7b3b623ff3f239eaad4e4bca545a7470f5b5e932
Daniel Schaaf <daniel.schaaf@outlook.com>
2021-06-02 17:58:46 -0700

    finished assignement 3 actually

--- Assignment3/src/com/ss/assignment3/AddingValues.java
+++ Assignment3/src/com/ss/assignment3/AddingValues.java
@@ -1,25 +1,26 @@
 package com.ss.assignment3;
 
 public class AddingValues {
-	 public static boolean isInteger(String s) {
+	 public static boolean isInteger(String string) {
 	      boolean isValid = false;
 	      try
 	      {
-	         Integer.parseInt(s);
+	         Integer.parseInt(string);
 	 
 	         // s is a valid integer
 	 
 	         isValid = true;
 	      }
-	      catch (NumberFormatException ex)
+	      catch (NumberFormatException e)
 	      {
-	         // s is not an integer
+	         // string is not an integer
 	      }
 	 
 	      return isValid;
 	   }
 	public static void main(String[] args) {
 		Integer sum = 0;
+		// loops thru the args and adds them up if they are integers. if they are strings it doesnt do anything with them.
 		for(int i = 0; i < args.length; i++) {
 			if(isInteger(args[i])) {
 				sum+= Integer.parseInt(args[i]);
