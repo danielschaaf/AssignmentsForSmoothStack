df736107d86dfad4456115c85180f5cc420a9e64
Daniel Schaaf <daniel.schaaf@outlook.com>
2021-06-04 10:22:37 -0700

    finished assignment 8 and fixed a typo in Assignment 5

--- Assignment5/src/com/ss/assignment5/ShapeProject.java
+++ Assignment5/src/com/ss/assignment5/ShapeProject.java
@@ -9,7 +9,7 @@ public class ShapeProject {
 		Rectangle r = new Rectangle();
 		System.out.println("the area of the triangle is "+ t.calcualateArea(4, 6));
 		System.out.println("the area of the circle is "+ c.calcualateArea(4, 4));
-		System.out.println("the area of the Rectangle is "+ t.calcualateArea(6,8 ));
+		System.out.println("the area of the Rectangle is "+ r.calcualateArea(6,8 ));
 	}
 
 }
