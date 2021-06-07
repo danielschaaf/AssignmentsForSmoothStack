17871ea769a05e7fba43b2f6b41fbb492f515553
Daniel Schaaf <daniel.schaaf@outlook.com>
2021-06-07 07:30:06 -0700

    finished deadlocking assignment

--- Assignemnt10/src/com/ss/assignment10/ThreadClass.java
+++
@@ -1,7 +0,0 @@
-package com.ss.assignment10;
-
-public class ThreadClass extends Thread {
-	public void run() {
-		System.out.println("Thread running");
-	}
-}
--- Assignemnt10/src/com/ss/assignment10/ThreadDeadlocking.java
+++ Assignemnt10/src/com/ss/assignment10/ThreadDeadlocking.java
@@ -2,13 +2,43 @@ package com.ss.assignment10;
 
 public class ThreadDeadlocking{
 	
+	
 	public static void main(String[] args) {
+		final String firstLock= "First Lock";
+		final String secondLock= "second Lock";
+		Thread thread1= new Thread() {
+			public void run() {
+				synchronized(firstLock){
+					System.out.println("thread 1: locked"+firstLock);
+					try{
+						Thread.sleep(100);
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+					synchronized(secondLock){
+						System.out.println("thread 1: locked"+secondLock);
+					}
+				}
+			}
+		};
 		
-		ThreadClass t1= new ThreadClass();
-		t1.start();
-		
-		ThreadClass t2 = new ThreadClass();
-		t2.start();
+		Thread thread2 = new Thread() {
+			public void run() {
+				synchronized(secondLock){
+					System.out.println("thread 2: locked"+secondLock);
+					try{
+						Thread.sleep(1000000);
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+				synchronized(firstLock){
+					System.out.println("thread 2: locked"+firstLock);
+				}
+			}
+		};
+		thread1.start();
+		thread2.start();
 	}
 	}
 
